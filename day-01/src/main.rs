2e01280ca5bc9251900b31b4eac1523ac2ec1e8d
Zilkos <zilkos@unixmail.fr>
2021-10-19 23:30:59 +0200

    Day 01 pretty easy with docs and rustbook !

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let mut sum_fuel = 0;
+    // read_to_string -> Result, so unwrap needed.
+    // lines return every line.
+    // Iterate over and display each item.
+    // As item is a string, parse it and give it to func.
+    for item in read_to_string("input").unwrap().lines() {
+        sum_fuel += calculate(item.parse().unwrap());
+    }
+
+    println!("{}", sum_fuel);
+}
+
+fn calculate(val: i32) -> i32 {
+    (val / 3) - 2
+}
+
+#[test]
+fn ok() {
+    assert_eq!(2, calculate(12));
+    assert_eq!(2, calculate(14));
+    assert_eq!(654, calculate(1969));
+    assert_eq!(33583, calculate(100756));
 }
